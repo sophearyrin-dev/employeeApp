fcab41da9deee48fad3cef58a4e6e3a881eeb10e
sophearyrin1 <srin@miu.edu>
2024-10-12 09:20:23 -0500

    config cros

--- employeemanager/src/main/java/tech/getarrays/employeemanager/model/Employee.java
+++ employeemanager/src/main/java/tech/getarrays/employeemanager/model/Employee.java
@@ -4,7 +4,7 @@ import javax.persistence.*;
 import java.io.Serializable;
 
 @Entity
-public class Employee implements Serializable {
+public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(nullable = false, updatable = false)
